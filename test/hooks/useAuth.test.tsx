7f4e0ef3e9ff7cc9b8b63fc7966b7ac87e4026ac
dorsha <dorsha@descope.com>
2022-11-01 00:31:17 +0200

    Fix lint

--- test/hooks/useAuth.test.tsx
+++ test/hooks/useAuth.test.tsx
@@ -22,7 +22,8 @@ const { logout } = createSdk({ projectId: '' });
 
 const authProviderWrapper =
 	(projectId: string) =>
-	({ children }: { children: any }) => <AuthProvider projectId={projectId}>{children}</AuthProvider>;
+	({ children }: { children: any }) =>
+		<AuthProvider projectId={projectId}>{children}</AuthProvider>;
 describe('useAuth', () => {
 	it('should throw error when used without provider', () => {
 		expect(() => {
