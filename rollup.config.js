590d33b2bc1ef193ae960ed51b179d234d2aa688
Asaf Shen <asaf@descope.com>
2022-11-24 13:37:33 +0200

    support cjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,80 @@
 import typescript from '@rollup/plugin-typescript';
-import del from 'rollup-plugin-delete';
+import fs from 'fs';
 import autoExternal from 'rollup-plugin-auto-external';
-import { terser } from 'rollup-plugin-terser';
-import dts from 'rollup-plugin-dts';
 import define from 'rollup-plugin-define';
+import del from 'rollup-plugin-delete';
+import dts from 'rollup-plugin-dts';
+import { terser } from 'rollup-plugin-terser';
 
 const packageJson = require('./package.json');
 
 export default [
 	{
 		input: 'src/lib/index.ts',
-		output: { dir: 'dist', format: 'esm' },
+		output: {
+			file: packageJson.main,
+			sourcemap: true,
+			format: 'cjs'
+		},
 		plugins: [
 			define({
 				replacements: {
 					BUILD_VERSION: JSON.stringify(require('./package.json').version)
 				}
 			}),
-			del({ targets: 'dist' }),
 			typescript({
-				rootDir: './src/lib'
+				tsconfig: './tsconfig.json'
 			}),
 			autoExternal(),
 			terser()
 		]
 	},
 	{
-		input: './dist/dts/index.d.ts',
+		input: 'src/lib/index.ts',
+		output: {
+			file: packageJson.module,
+			sourcemap: true,
+			format: 'esm'
+		},
+		plugins: [
+			define({
+				replacements: {
+					BUILD_VERSION: JSON.stringify(require('./package.json').version)
+				}
+			}),
+			typescript({
+				tsconfig: './tsconfig.json'
+			}),
+			autoExternal(),
+			terser()
+		]
+	},
+	{
+		input: './dist/src/lib/index.d.ts',
 		output: [{ file: packageJson.types, format: 'esm' }],
-		plugins: [dts(), del({ hook: 'buildEnd', targets: ['./dist/dts'] })]
+		plugins: [
+			dts(),
+			del({
+				hook: 'buildEnd',
+				targets: [
+					'./dist/test',
+					'./dist/src',
+					'./dist/cjs/!(index.cjs.*|package.json)'
+				]
+			}),
+			cjsPackage()
+		]
 	}
 ];
+
+function cjsPackage() {
+	return {
+		name: 'cjsPackage',
+		buildEnd: () => {
+			fs.writeFileSync(
+				'./dist/cjs/package.json',
+				JSON.stringify({ type: 'commonjs' })
+			);
+		}
+	};
+}
