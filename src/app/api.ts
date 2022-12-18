4ec768e41250348e554f3053118aba7bdea96081
Asaf Shen <asaf@descope.com>
2022-12-18 21:53:44 +0200

    commit file

---
+++ src/app/api.ts
@@ -0,0 +1,20 @@
+/* eslint-disable import/prefer-default-export */
+import { getSessionToken } from '../lib';
+
+// In this sample app, we just call an external public API
+// You will probably want to replace it with your backend URL
+const apiUrl = 'https://uselessfacts.jsph.pl/random.json?language=en';
+
+// This is an example for using ,getSessionToken, in utility function
+// This is useful to pass session token to your backend
+// Note: Descope backend SDKs support extracting session token from the Authorization header
+export const fetchData = async () => {
+	const sessionToken = getSessionToken();
+	const res = await fetch(apiUrl, {
+		headers: {
+			Authorization: `Bearer ${sessionToken}`
+		}
+	});
+	const data = await res.json();
+	return data?.text;
+};
