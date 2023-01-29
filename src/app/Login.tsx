96281bf4d6af413518e62fdec68b65f7877e2b92
talaharoni <tal@descope.com>
2023-01-29 11:44:48 +0200

    fix review suggestion

--- src/app/Login.tsx
+++ src/app/Login.tsx
@@ -22,7 +22,7 @@ const Login = () => {
 				onError={onError}
 				debug={process.env.DESCOPE_DEBUG_MODE === 'true'}
 				theme={process.env.DESCOPE_THEME as DescopeTheme}
-				telemetryKey={process.env.DESCOPE_TELEMETRY_KEY || undefined}
+				telemetryKey={process.env.DESCOPE_TELEMETRY_KEY}
 			/>
 			{errorMessage && (
 				<div
