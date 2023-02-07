118c480941a26e2b6774e1a3d7b43b6ba2d0f74d
Nir Gur Arie <nir@descope.com>
2023-02-07 21:29:37 +0200

    fix session loading indication

--- src/lib/hooks/useSession.ts
+++ src/lib/hooks/useSession.ts
@@ -1,17 +1,27 @@
-import { useEffect } from 'react';
+import { useMemo, useRef } from 'react';
 import useContext from './useContext';
 
 const useSession = () => {
 	const { session, isSessionLoading, fetchSession } = useContext();
+	// when session should be received, we want "isSessionLoading" to be true starting from the first call
+	// (and not only when receiving an update from the context)
+	const shouldGetSession = useRef(isSessionLoading);
 
-	useEffect(() => {
+	// we want this to happen before returning a value so we are using "useMemo" and not "useEffect"
+	useMemo(() => {
+		shouldGetSession.current = isSessionLoading;
+	}, [isSessionLoading]);
+
+	// we want this to happen before returning a value so we are using "useMemo" and not "useEffect"
+	useMemo(() => {
 		if (!session && !isSessionLoading) {
+			shouldGetSession.current = true;
 			fetchSession();
 		}
 	}, [fetchSession]);
 
 	return {
-		isSessionLoading,
+		isSessionLoading: shouldGetSession.current,
 		sessionToken: session,
 		isAuthenticated: !!session
 	};
