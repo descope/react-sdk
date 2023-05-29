4a96e5ad272b11b7e192d334b7099d562a331f8c
Asaf Shen <asaf@descope.com>
2023-05-29 10:30:36 +0300

    reproduce refresh loop

--- examples/app/App.tsx
+++ examples/app/App.tsx
@@ -1,38 +1,16 @@
 import React from 'react';
-import { Outlet, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
-import StepUp from './StepUp';
+import { useSession } from '../../src';
 
-const Layout = () => (
-	<div style={{ height: '100vh', position: 'relative' }}>
-		<div
-			style={{
-				borderRadius: 10,
-				margin: 'auto',
-				border: '1px solid lightgray',
-				padding: 20,
-				maxWidth: '400px',
-				boxShadow: '13px 13px 20px #cbced1, -13px -13px 20px #fff',
-				background: '#ecf0f3',
-				position: 'relative',
-				top: '50%',
-				transform: 'translateY(-50%)'
-			}}
-		>
-			<Outlet />
-		</div>
-	</div>
-);
+const App = () => {
+	const { isSessionLoading, isAuthenticated } = useSession();
 
-const App = () => (
-	<Routes>
-		<Route element={<Layout />}>
-			<Route index element={<Home />} />
-			<Route path="/login" element={<Login />} />
-			<Route path="/step-up" element={<StepUp />} />
-		</Route>
-	</Routes>
-);
+	if (isSessionLoading) {
+		return <div>Loading...</div>;
+	}
+
+	return <>{isAuthenticated ? <div>login</div> : <Home />}</>;
+};
 
 export default App;
--- examples/app/index.tsx
+++ examples/app/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from '../../src';
 import App from './App';
 
@@ -9,12 +8,10 @@ const container = document.getElementById('root');
 const root = createRoot(container!);
 
 root.render(
-	<BrowserRouter>
-		<AuthProvider
-			projectId={process.env.DESCOPE_PROJECT_ID}
-			baseUrl={process.env.DESCOPE_BASE_URL}
-		>
-			<App />
-		</AuthProvider>
-	</BrowserRouter>
+	<AuthProvider
+		projectId={process.env.DESCOPE_PROJECT_ID}
+		baseUrl={process.env.DESCOPE_BASE_URL}
+	>
+		<App />
+	</AuthProvider>
 );
